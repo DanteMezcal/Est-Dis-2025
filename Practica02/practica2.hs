371b3d813f4e9829453087069a63e674484e927e
DanteMezcal <322037461@ciencias.unam.mx>
2024-09-05 21:31:00 -0600

    Hice algunas funciones

--- Practica02/practica2.hs
+++ Practica02/practica2.hs
@@ -2,15 +2,16 @@ longitud :: [a] -> Int
 longitud [] = 0
 longitud (x:xs) = 1 + longitud xs
 
-sumaLista :: [Int] -> Int
+sumaLista :: Num x => [x] -> x
 sumaLista [] = 0
 sumaLista (x:xs) = x + sumaLista xs
 
 agregarElemento :: [a] -> a -> Bool -> [a]
-agregarElemento (x:xs) a trueFalse =
-    if trueFalse
-    then a:(x:xs)
-    else (x:xs) ++ [a]
+agregarElemento [] elemento boolean = [elemento]
+agregarElemento (x:xs) elemento boolean =
+    if boolean
+    then elemento : (x:xs)    
+    else (x:xs) ++ [elemento] 
 
 maximoLista :: (Num a, Ord a) => [a] -> a
 maximoLista [] = 0
@@ -20,5 +21,19 @@ maximoLista (x:xs) =
   else maximoLista xs
 
 indice :: [a] -> Int -> a
-indice [] = 0
-indice (x:xs) = 
+indice (x:xs) 1 = x
+indice (x:xs) i = indice xs (i - 1)
+
+divisores :: Int -> [Int]
+divisores n = [x | x <- [1..n], n `mod` x == 0]
+
+conjunto :: Eq a => [a] -> [a]
+conjunto [] = []
+conjunto (x:xs) = x : conjunto [z | z <- xs, z /= x]
+
+--numerosPares :: Num a => [a] -> [a]
+--numerosPares [] = []
+--numerosPares (x:xs) = [x | x <- [a], x]
+
+--prueba :: Int -> Int -> Int
+--prueba x y = x `div` y 
