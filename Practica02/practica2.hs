da882289ba909600ca350d24351389e61451ed7d
DanteMezcal <322037461@ciencias.unam.mx>
2024-09-18 19:23:58 -0600

    Termine junto con frida las funciones, lo hicimos entre los dos porque somos unos muy buenos estudiantes y amiguitos que se esfuerzan en las funciones de haskell :)

--- Practica02/practica2.hs
+++ Practica02/practica2.hs
@@ -1,39 +1,44 @@
 longitud :: [a] -> Int
 longitud [] = 0
-longitud (x:xs) = 1 + longitud xs
+longitud (x:xs) = 1 + longitud(xs)
 
-sumaLista :: Num x => [x] -> x
+sumaLista :: Num a => [a] -> a
 sumaLista [] = 0
-sumaLista (x:xs) = x + sumaLista xs
+sumaLista (x:xs) = x + sumaLista(xs)
 
-agregarElemento :: [a] -> a -> Bool -> [a]
-agregarElemento [] elemento boolean = [elemento]
-agregarElemento (x:xs) elemento boolean =
-    if boolean
-    then elemento : (x:xs)    
-    else (x:xs) ++ [elemento] 
+agregaElemento :: [a] -> a -> Bool -> [a]
+agregaElemento [] z b = [z]
+agregaElemento (x:xs) z b = if b
+  then z:(x:xs)
+  else (x:xs) ++ [z]
 
 maximoLista :: (Num a, Ord a) => [a] -> a
 maximoLista [] = 0
 maximoLista (x:xs) =
-  if x > maximoLista xs
+  if x > maximoLista(xs)
   then x
-  else maximoLista xs
-
+  else maximoLista(xs)
+  
 indice :: [a] -> Int -> a
-indice (x:xs) 1 = x
-indice (x:xs) i = indice xs (i - 1)
+indice [] z = error "El indice esta fuera del rango o la lista esta vacia"
+indice (x:xs) 0 = x
+indice (x:xs) z =
+  if z <= longitud(x:xs)
+  then indice(xs) (z - 1)
+  else error "El indice esta fuera del rango de la lista"
+  
+--indice :: [a] -> Int -> a
+--indice (x:xs) 1 = x
+--indice (x:xs) i = indice xs (i - 1)
+
 
 divisores :: Int -> [Int]
+divisores 0 = []
 divisores n = [x | x <- [1..n], n `mod` x == 0]
 
 conjunto :: Eq a => [a] -> [a]
 conjunto [] = []
-conjunto (x:xs) = x : conjunto [z | z <- xs, z /= x]
+conjunto (x:xs) = x : conjunto [z | z <- (xs), x /= z]
 
---numerosPares :: Num a => [a] -> [a]
---numerosPares [] = []
---numerosPares (x:xs) = [x | x <- [a], x]
-
---prueba :: Int -> Int -> Int
---prueba x y = x `div` y 
+numerosPares :: [Int] -> [Int]
+numerosPares (x:xs) = [z | z <- (x:xs), z `mod` 2 == 0 ]
